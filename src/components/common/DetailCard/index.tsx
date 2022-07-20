b97b4eb69ce259dc602f80b5d596d15325f702b4
Carl Welch <carlwelchdesign@gmail.com>
2022-07-19 19:12:54 -0700

    clean up

--- src/components/common/DetailCard/index.tsx
+++ src/components/common/DetailCard/index.tsx
@@ -7,44 +7,33 @@ import { grey } from '@mui/material/colors'
 import StarRating from '../StarRating'
 import { UserReviewContent, AuthorDateContainer } from '../StyledComponents'
 import InsertCommentIcon from '@mui/icons-material/InsertComment';
-
-
-
-
 interface Props {
-    reviewDetail: ReviewDataType
-    showCommentButton: boolean
-    handleModal: () => void
-  }
+  reviewDetail: ReviewDataType
+  showCommentButton: boolean
+  handleModal: () => void
+}
   
-  type DetailProps = CommentFormDataTypes & Props
-
-
-
-
+type DetailProps = CommentFormDataTypes & Props
 
 const DetailCard = ({reviewDetail, showCommentButton, handleModal }: DetailProps) => {
-
-    return (
-        <>
-        <ReviewCardDetailContainer>
-        <Typography variant={'h6'} sx={{ fontSize: 16, fontWeight: 600 }} color="text.primary">
-          {reviewDetail?.place}
-        </Typography>
-        <StarRating {...{ rating: reviewDetail?.rating || 0 }} />
-        <UserReviewContent sx={{ fontSize: 13}} color="text.secondary">
-          {reviewDetail?.content}
-        </UserReviewContent>
-        <AuthorDateContainer>
-          <Typography sx={{ fontSize: 10, textAlign: 'left', marginRight: '40px' }} color="text.primary">{reviewDetail?.author}</Typography>
-          <Typography sx={{ fontSize: 10, textAlign: 'right', color: grey[500] }}>{day(reviewDetail?.published_at).format('DD/MM/YYYY')}</Typography>
-        </AuthorDateContainer>
-        {showCommentButton && <AddCommentButton color="primary" aria-label="Add Comment" onClick={handleModal}>
-          <InsertCommentIcon sx={{ fontSize: 16, color: grey[800]}}/>
-        </AddCommentButton>}
-      </ReviewCardDetailContainer>
-        </>
-    )
+  return (
+    <ReviewCardDetailContainer>
+      <Typography variant={'h6'} sx={{ fontSize: 16, fontWeight: 600 }} color="text.primary">
+        {reviewDetail?.place}
+      </Typography>
+      <StarRating {...{ rating: reviewDetail?.rating || 0 }} />
+      <UserReviewContent sx={{ fontSize: 13}} color="text.secondary">
+        {reviewDetail?.content}
+      </UserReviewContent>
+      <AuthorDateContainer>
+        <Typography sx={{ fontSize: 10, textAlign: 'left', marginRight: '40px' }} color="text.primary">{reviewDetail?.author}</Typography>
+        <Typography sx={{ fontSize: 10, textAlign: 'right', color: grey[500] }}>{day(reviewDetail?.published_at).format('DD/MM/YYYY')}</Typography>
+      </AuthorDateContainer>
+      {showCommentButton && <AddCommentButton color="primary" aria-label="Add Comment" onClick={handleModal}>
+        <InsertCommentIcon sx={{ fontSize: 16, color: grey[800]}}/>
+      </AddCommentButton>}
+    </ReviewCardDetailContainer>
+  )
 }
 
 export default DetailCard
