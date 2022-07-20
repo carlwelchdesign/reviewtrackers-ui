8990f4b8e822b3e8e99cd50d87b034f2032a72eb
Carl Welch <carlwelchdesign@gmail.com>
2022-07-19 17:39:13 -0700

    clean up

--- src/components/common/ReviewCard/index.tsx
+++ src/components/common/ReviewCard/index.tsx
@@ -7,8 +7,6 @@ import { Link } from 'react-router-dom'
 import { Card, Typography } from '@mui/material'
 import { grey } from '@mui/material/colors'
 import { UserReviewListContent, AuthorAndDateSubCantainer } from '../StyledComponents'
-// import ForumIcon from '@mui/icons-material/Forum'
-// import {BrowserRouter as Router} from 'react-router-dom';
 
 type Props = {
   review: ReviewDataType
@@ -17,23 +15,21 @@ type Props = {
 const ReviewCard = ({review}: Props) => {
   const { id, place, rating, content, author, published_at } = review
   return (
-    // <Router>
-      <CardContainer>
-        <LinkWrapper to={`/details/${id}`}>
-          <Typography variant={'h6'} sx={{ fontSize: 16, fontWeight: 600 }} color="text.primary">
-            {place}
-          </Typography>
-          <StarRating {...{ rating: rating }} />
-          <UserReviewListContent sx={{ fontSize: 13 }} color="text.secondary">
-            {content}
-          </UserReviewListContent>
-          <AuthorAndDateSubCantainer>
-            <Typography sx={{ fontSize: 10, textAlign: 'left', width: '50%'}} color="text.primary">{author} </Typography>
-            <Typography sx={{ fontSize: 10, textAlign: 'right', width: '50%', color: grey[500] }} >{day(published_at).format('DD/MM/YYYY')}</Typography>
-          </AuthorAndDateSubCantainer>
-          </LinkWrapper>
-      </CardContainer>
-    // </Router>
+    <CardContainer>
+      <LinkWrapper to={`/details/${id}`}>
+        <Typography variant={'h6'} sx={{ fontSize: 16, fontWeight: 600 }} color="text.primary">
+          {place}
+        </Typography>
+        <StarRating {...{ rating: rating }} />
+        <UserReviewListContent sx={{ fontSize: 13 }} color="text.secondary">
+          {content}
+        </UserReviewListContent>
+        <AuthorAndDateSubCantainer>
+          <Typography sx={{ fontSize: 10, textAlign: 'left', width: '50%'}} color="text.primary">{author} </Typography>
+          <Typography sx={{ fontSize: 10, textAlign: 'right', width: '50%', color: grey[500] }} >{day(published_at).format('DD/MM/YYYY')}</Typography>
+        </AuthorAndDateSubCantainer>
+      </LinkWrapper>
+    </CardContainer>
   )
 }
 
