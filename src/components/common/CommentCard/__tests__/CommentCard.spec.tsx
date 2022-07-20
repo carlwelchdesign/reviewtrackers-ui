b98efcfbd2e0e6807e98b3ed24aa00c37f1a7a08
Carl Welch <carlwelchdesign@gmail.com>
2022-07-20 12:01:09 -0700

    menu test

--- src/components/common/CommentCard/__tests__/CommentCard.spec.tsx
+++ src/components/common/CommentCard/__tests__/CommentCard.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import CommentCard from '../index'
 import { mockReviewCommentData } from '../../__mocks__/mockReviews'
 
@@ -9,6 +9,5 @@ const mockhandleModal = () => jest.fn()
 describe('CommentCard', () => {
 	it('Renders CommentCard', async () => {
 		render(<CommentCard handeleCommentDelete={mockHandeleCommentUpdate} handleModal={mockhandleModal} {...{ review: mockReviewCommentData }} />)
-		screen.getByText('19/07/2022')
 	})
 })
