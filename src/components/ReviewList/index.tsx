18553639489f759f15036e2da4821a127d0070c6
Carl Welch <carlwelchdesign@gmail.com>
2022-07-20 22:42:29 -0700

    add grid for responsive layout of list

--- src/components/ReviewList/index.tsx
+++ src/components/ReviewList/index.tsx
@@ -17,7 +17,11 @@ const ReviewList = () => {
 
   return (
     <React.Suspense fallback="loading...">
-      <Grid container spacing={{ xs: 3, md: 4 }} columns={{ xs: 1, sm: 8, md: 12, lg: 18}}>
+      <Grid 
+        alignItems="center"
+        justifyContent="center" 
+        container spacing={{ xs: 3, md: 4 }} 
+        columns={{ xs: 1, sm: 8, md: 12, lg: 18}}>
         {reviewList.map((review: ReviewDataType, index: number) => 
           <Grid item xs={2} sm={4} md={4} key={index}>
             <ReviewCard key={review.id} {...{review}} />
